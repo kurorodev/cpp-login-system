05335e2d13063d793d3f98cc0c1fc5fd7461896d
Daniil Kudryavcev <83886080+kurorodev@users.noreply.github.com>
2022-08-04 22:21:16 +0400

    shitcoding :)

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool IsLoggedIn()
+{
+    string username, password, un, pw;
+
+    cout << "Enter username: "; cin >> username;
+    cout << "Enter password: "; cin >> password;
+
+    ifstream read("data\\" + username + password + ".txt");
+    getline(read, un);
+    getline(read, pw);
+
+    if (un == username && pw == password)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int choice;
+
+    cout << "1: Register\n2: Login\nYour choice "; cin >> choice;
+    if (choice == 1)
+    {
+        string username, password;
+        cout << "select a username: "; cin >> username;
+        cout << "select a password: "; cin >> password;
+
+        ofstream file;
+        file.open("data\\" + username + ".txt");
+        file << username << endl << password;
+        file.close();
+
+        main();
+    }
+    else if (choice == 2)
+    {
+        bool status = IsLoggedIn();
+
+        if (!status)
+        {
+            cout << "False login!" << endl;
+            system("PAUSE");
+            return 0;
+        }
+        else 
+        {
+            cout << "Succesfuly login!" << endl;
+            system("PAUSE");
+            return 1;
+        }
+    }
+}
